72e60bc7e02590eff3b208f24be5f67cd2c9430c
bajajku <bajajku@sheridancollege.ca>
2024-10-19 22:40:56 -0400

    Added pet model.

---
+++ lib/Models/pet.dart
@@ -0,0 +1,16 @@
+class Pet {
+
+  // Field variables for Pet, right now basic might change later.
+  final String id;
+  final String name;
+  final String breed;
+  final DateTime birthDate;
+
+  // Constructor to create Pet
+  Pet({
+    required this.id,
+    required this.name,
+    required this.breed,
+    required this.birthDate,
+  });
+}
